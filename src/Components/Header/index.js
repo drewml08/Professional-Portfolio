6a46918af3b3da7e20732d1f89e8d225c9de46e5
Mike Drew <drewml08@gmail.com>
2022-01-29 22:54:19 -0500

    finished navbar functionality with scrollspy, scroll duration is broken for this component and will test another one

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,44 +2,48 @@ import React from "react";
 import ScrollSpy from "react-scrollspy-navigation";
 import "./style.css";
 
+
+
 function Header() {
     return (
         <header className="fixed-top">
             <nav id="nav-main" className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="#"></a>
-                <ScrollSpy offsetTop={120}>
-                    <a
-                        className="nav-link"
-                        href="#About"
-                        ref={React.createRef()}
-                    >
-                        About Me
-                    </a>
+                <ul className="nav nav-pills">
+                    <ScrollSpy duration={100} offsetTop={120}>
+                        <a
+                            className="nav-link"
+                            href="#About"
+                            ref={React.createRef()}
+                        >
+                            About Me
+                        </a>
 
-                    <a
-                        className="nav-link"
-                        href="#Resume"
-                        ref={React.createRef()}
-                    >
-                        Resume
-                    </a>
+                        <a
+                            className="nav-link"
+                            href="#Resume"
+                            ref={React.createRef()}
+                        >
+                            Resume
+                        </a>
 
-                    <a
-                        className="nav-link"
-                        href="#Portfolio"
-                        ref={React.createRef()}
-                    >
-                        Portfolio
-                    </a>
+                        <a
+                            className="nav-link"
+                            href="#Portfolio"
+                            ref={React.createRef()}
+                        >
+                            Portfolio
+                        </a>
 
-                    <a
-                        className="nav-link"
-                        href="#Contact"
-                        ref={React.createRef()}
-                    >
-                        Contact
-                    </a>
-                </ScrollSpy>
+                        <a
+                            className="nav-link"
+                            href="#Contact"
+                            ref={React.createRef()}
+                        >
+                            Contact
+                        </a>
+                    </ScrollSpy>
+                </ul>
             </nav>
         </header>
     );
@@ -104,5 +108,32 @@ export default Header;
                     </a>
                 </ScrollSpy>
 
+
+
+                  <NavButton
+                            link="#About"
+                            text="About Me"
+                            ref={React.createRef()}
+                        />
+                        <NavButton
+                            link="#Resume"
+                            text="Resume"
+                            ref={React.createRef()}
+                        />
+                        <NavButton
+                            link="#Portfolio"
+                            text="Portfolio"
+                            ref={React.createRef()}
+                        />
+                        <NavButton
+                            link="#Contact"
+                            text="Contact"
+                            ref={React.createRef()}
+                        />
+const NavButton = React.forwardRef(({ href, text }, ref) => (
+    <a className="nav-link" href={href} ref={ref}>
+        {text}
+    </a>
+));
 
                 */
